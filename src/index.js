360449aae04e1ea94de8301352af528949b7f687
BigbrotherShin <shinjhhp5@gmail.com>
2019-12-23 22:15:31 +0900

    make redux module

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './exercise';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'; //리액트 프로젝트에 리덕스를 적용 할 때에는 react-redux 라는 라이브러리를 사용
+import rootReducer from './modules';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer); // make store, store function parameter is reducer
+
+ReactDOM.render( // Provider의 props로 store를 넣어서 App 을 감싸게 되면 
+  // 렌더링하는 그 어떤 컴포넌트던지 리덕스 스토어에 접근 할 수 있음.
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
