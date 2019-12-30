e07fefbe3b97129d2521263d0c4d6069c44e5bff
BigbrotherShin <shinjhhp5@gmail.com>
2019-12-30 18:37:35 +0900

    refactoring 3

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,18 +1,24 @@
 import React, { useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { connect } from 'react-redux';
 import Todos from '../components/Todos';
 import { addTodo, toggleTodo } from '../modules/todos';
 
-function TodosContainer() {
-  // useSelector 에서 꼭 객체를 반환 할 필요는 없습니다.
-  // 한 종류의 값만 조회하고 싶으면 그냥 원하는 값만 바로 반환하면 됩니다.
-  const todos = useSelector(state => state.todos);
-  const dispatch = useDispatch();
+// mapStateToProps와 mapDispatchToProps를 따로 선언하지 않고 
+// connect 함수를 사용 할 때 인자 쪽에서 익명함수로 바로 만들어서 사용하면 코드가 꽤나 깔끔
 
-  const onCreate = useCallback(text => dispatch(addTodo(text)), [dispatch]);
-  const onToggle = useCallback(id => dispatch(toggleTodo(id)), [dispatch]); // 최적화를 위해 useCallback 사용
-
+function TodosContainer({ todos, addTodo, toggleTodo }) {
+  const onCreate = text => addTodo(text);
+  const onToggle = useCallback(id => toggleTodo(id), [toggleTodo]); // 최적화를 위해 useCallback 사용
+  
   return <Todos todos={todos} onCreate={onCreate} onToggle={onToggle} />;
 }
 
-export default TodosContainer;
+export default connect(
+  state => ({ todos: state.todos }),
+  {
+    addTodo,
+    toggleTodo
+  }
+)(TodosContainer);
+
+// connect를 쓸 때에는 이런 형태를 선호합니다만 꼭 이렇게 할 필요는 없습니다.
