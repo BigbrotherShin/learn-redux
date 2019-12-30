e6ea70bedbb75890dc87b69900a6a199ba0fbfb9
BigbrotherShin <shinjhhp5@gmail.com>
2019-12-30 18:30:42 +0900

    HOC bindActionCreators Refactoring 1

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -2,20 +2,23 @@
 // 이는 리액트 컴포넌트를 개발하는 하나의 패턴으로써, 컴포넌트의 로직을 재활용 할 때 유용한 패턴
 // HOC의 용도는 "컴포넌트를 특정 함수로 감싸서 특정 값 또는 함수를 props로 받아와서 사용 할 수 있게 해주는 패턴"이라는 것 정도
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease, setDiff } from '../modules/counter';
 
-function CounterContainer({ number, diff, onIncrease, onDecrease, onSetDiff }) {
+// 액션 생성함수 이름이 바뀌어서 props 이름도 바뀌었습니다.
+// 예: onIncrease -> increase
+function CounterContainer({ number, diff, increase, decrease, setDiff }) {
   return (
     <Counter
       // 상태와
       number={number}
       diff={diff}
       // 액션을 디스패치 하는 함수들을 props로 넣어줍니다.
-      onIncrease={onIncrease}
-      onDecrease={onDecrease}
-      onSetDiff={onSetDiff}
+      onIncrease={increase}
+      onDecrease={decrease}
+      onSetDiff={setDiff}
     />
   );
 }
@@ -29,11 +32,17 @@ const mapStateToProps = state => ({
 
 // mapDispatchToProps 는 액션을 디스패치하는 함수를 만들어서 props로 넣어줍니다.
 // dispatch 를 파라미터로 받아옵니다.
-const mapDispatchToProps = dispatch => ({
-  onIncrease: () => dispatch(increase()),
-  onDecrease: () => dispatch(decrease()),
-  onSetDiff: diff => dispatch(setDiff(diff)),
-});
+const mapDispatchToProps = dispatch => (
+  // bindActionCreators 를 사용하면, 자동으로 액션 생성 함수에 dispatch 가 감싸진 상태로 호출 할 수 있습니다.
+  bindActionCreators(
+    {
+      increase,
+      decrease,
+      setDiff
+    },
+    dispatch
+  )
+);
 
 // connect 함수에는 mapStateToProps, mapDispatchToProps 를 인자로 넣어주세요.
 export default connect(
